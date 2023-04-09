a786eb71d61f5c1550e6f1571b9c007107c2d1cf
hactazoff <113508403+hactazoff@users.noreply.github.com>
2023-04-09 13:12:03 +0200

    add dist path

---
+++ dist/src/client.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Client = void 0;
+class Client {
+    constructor(args) {
+        console.log(args);
+    }
+}
+exports.Client = Client;
---
+++ dist/src/updater.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Updater = void 0;
+class Updater {
+    constructor(args) {
+        console.log(args);
+    }
+}
+exports.Updater = Updater;
